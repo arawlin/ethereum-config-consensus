bac479c6ac6aa4e810dc1fe4df1ec2b8018b733f
arawlin <arawlinlin@gmail.com>
2025-06-11 21:16:53 +0800

    log info

--- start.sh
+++ start.sh
@@ -7,5 +7,5 @@ $DIR_DATA/stop.sh
 echo -e "Starting \n"
 
 #../lighthouse/target/maxperf/lighthouse bn --network mainnet --disable-deposit-contract-sync --datadir $DIR_DATA --checkpoint-sync-url https://sync-mainnet.beaconcha.in/ --http --execution-endpoint=http://localhost:8551 --execution-jwt ../jwt.hex --logfile $DIR_DATA/logs/lighthouse.log > /dev/null 2>&1 &
-../lighthouse/target/maxperf/lighthouse bn --network mainnet --disable-deposit-contract-sync --datadir $DIR_DATA --allow-insecure-genesis-sync --http --execution-endpoint=http://localhost:8551 --execution-jwt ../jwt.hex --logfile $DIR_DATA/logs/lighthouse.log > /dev/null 2>&1 &
+../lighthouse/target/maxperf/lighthouse bn --network mainnet --disable-deposit-contract-sync --datadir $DIR_DATA --allow-insecure-genesis-sync --http --execution-endpoint=http://localhost:8551 --execution-jwt ../jwt.hex --logfile $DIR_DATA/logs/lighthouse.log --logfile-debug-level info > /dev/null 2>&1 &
 echo $! >$DIR_DATA/node.pid
